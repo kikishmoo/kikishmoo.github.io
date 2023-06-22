abd1c8977d2bf01e0ba3407c87a4d643727fe9db
Kiki Greene <403540223@qq.com>
2023-06-21 23:58:25 -0400

    Update trivia.js

--- js/trivia.js
+++ js/trivia.js
@@ -1,8 +1,9 @@
+// Array of trivia question objects
 var trivia = [
     { 
         question: "What is the capital of France?", 
         choices: ["London", "New York", "Paris", "Sydney"],
-        answer: 2  // refers to the index of the correct answer in choices
+        answer: 2  // Index of the correct answer in the choices array
     },
     { 
         question: "Who wrote 'To Kill a Mockingbird'?", 
@@ -16,28 +17,43 @@ var trivia = [
     }
 ];
 
+// Function to run when the page has finished loading
 window.onload = function() {
+    // Add a click event listener to the "generate" button
     document.getElementById("generate").addEventListener("click", function() {
+        // Generate a random index for selecting a trivia question
         var randomIndex = Math.floor(Math.random() * trivia.length);
+        // Select a trivia question using the random index
         var triviaItem = trivia[randomIndex];
 
+        // Display the question
         document.getElementById("question").innerText = triviaItem.question;
+        // Hide the result message
         document.getElementById("result").classList.add("hidden");
         
+        // Get the "choices" div
         var choicesElement = document.getElementById("choices");
-        choicesElement.innerHTML = '';  // Clear any existing choices
+        // Clear any existing choices
+        choicesElement.innerHTML = '';
+        // Loop through the choices for the trivia question
         triviaItem.choices.forEach(function(choice, index) {
-            var li = document.createElement("li");
-            li.innerText = choice;
-            li.addEventListener("click", function() {
+            // Create a new button for each choice
+            var button = document.createElement("button");
+            // Set the text of the button to the choice
+            button.innerText = choice;
+            // Add a click event listener to the button
+            button.addEventListener("click", function() {
+                // Check whether the choice is correct
                 if (index === triviaItem.answer) {
+                    // If the choice is correct, display a success message
                     document.getElementById("result").innerText = "Correct!";
                 } else {
+                    // If the choice is incorrect, display a failure message
                     document.getElementById("result").innerText = "Sorry, that's incorrect.";
                 }
+                // Show the result message
                 document.getElementById("result").classList.remove("hidden");
             });
-            choicesElement.appendChild(li);
-        });
-    });
-};
+            // Add the button to the "choices" div
+            choicesElement.appendChild(button);
+ 
