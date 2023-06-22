f70b239091d2f281ddd9d365de11794e252ad255
Kiki Greene <403540223@qq.com>
2023-06-21 23:29:39 -0400

    Update trivia.js

--- js/trivia.js
+++ js/trivia.js
@@ -1,25 +1,43 @@
-// Array of trivia questions and answers
 var trivia = [
-    { question: "What is the capital of France?", answer: "Paris" },
-    { question: "Who wrote 'To Kill a Mockingbird'?", answer: "Harper Lee" },
-    { question: "What is the symbol for gold on the periodic table?", answer: "Au" }
-    // Add more questions as desired
+    { 
+        question: "What is the capital of France?", 
+        choices: ["London", "New York", "Paris", "Sydney"],
+        answer: 2  // refers to the index of the correct answer in choices
+    },
+    { 
+        question: "Who wrote 'To Kill a Mockingbird'?", 
+        choices: ["Mark Twain", "Harper Lee", "Ernest Hemingway", "George Orwell"], 
+        answer: 1
+    },
+    { 
+        question: "What is the symbol for gold on the periodic table?", 
+        choices: ["Ag", "Au", "Fe", "O"],
+        answer: 1
+    }
 ];
 
 window.onload = function() {
-    // When the Generate button is clicked...
     document.getElementById("generate").addEventListener("click", function() {
-        // Choose a random trivia question
         var randomIndex = Math.floor(Math.random() * trivia.length);
         var triviaItem = trivia[randomIndex];
 
-        // Display the question and answer
         document.getElementById("question").innerText = triviaItem.question;
-        document.getElementById("answer").innerText = "Click for the answer";
+        document.getElementById("result").classList.add("hidden");
         
-        // When the answer is clicked, reveal it
-        document.getElementById("answer").addEventListener("click", function() {
-            this.innerText = triviaItem.answer;
+        var choicesElement = document.getElementById("choices");
+        choicesElement.innerHTML = '';  // Clear any existing choices
+        triviaItem.choices.forEach(function(choice, index) {
+            var li = document.createElement("li");
+            li.innerText = choice;
+            li.addEventListener("click", function() {
+                if (index === triviaItem.answer) {
+                    document.getElementById("result").innerText = "Correct!";
+                } else {
+                    document.getElementById("result").innerText = "Sorry, that's incorrect.";
+                }
+                document.getElementById("result").classList.remove("hidden");
+            });
+            choicesElement.appendChild(li);
         });
     });
 };
